51fec7dbb68876c0eb5dd9d540bc15c536a24ef8
zhangli20080808 <791492434@qq.com>
2021-10-20 18:37:42 +0800

    配置

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,29 +1,29 @@
-import vue from 'rollup-plugin-vue'
-import css from 'rollup-plugin-css-only'
-import typescript from 'rollup-plugin-typescript2'
-import { nodeResolve } from '@rollup/plugin-node-resolve'
-import { name } from '../package.json'
-const file = type => `dist/${name}.${type}.js`
+import vue from 'rollup-plugin-vue';
+import css from 'rollup-plugin-css-only';
+import typescript from 'rollup-plugin-typescript2';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { name } from '../package.json';
+const file = (type) => `dist/${name}.${type}.js`;
 const overrides = {
   // 生成.d.ts文件
-  compilerOptions: { declaration: true }, 
+  compilerOptions: { declaration: true },
   // 排除文件
-  exclude: ["tests/**/*.ts", "tests/**/*.tsx"]
-}
-export { name, file }
+  exclude: ['tests/**/*.ts', 'tests/**/*.tsx'],
+};
+export { name, file };
 export default {
   input: 'src/index.ts',
   output: {
     name,
     file: file('esm'),
-    format: 'es'
+    format: 'es',
   },
   plugins: [
     nodeResolve(),
     typescript({ tsconfigOverride: overrides }),
     vue(),
-    css({ output: 'bundle.css' })
+    css({ output: 'bundle.css' }),
   ],
   // 排除的打包选项
-  external: ['vue', 'lodash-es']
-}
+  external: ['vue', 'lodash-es'],
+};
